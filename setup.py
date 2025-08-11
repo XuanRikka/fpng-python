756a2d7a32e85bf39b383b2475e9ad42a5592555
Rikka-desu <2273379351@qq.com>
2025-08-11 23:03:25 +0800

    Refactor setup.py: dynamic compiler flags

--- setup.py
+++ setup.py
@@ -1,23 +1,39 @@
 from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
 import platform
 import numpy
 
-# Compiler flags
-extra_compile_args = ["-fno-strict-aliasing"]
-if platform.processor() == "x86_64":
-    extra_compile_args += ["-msse4.1", "-mpclmul"]
 
-# Define the extension module
+class BuildExt(build_ext):
+    def build_extensions(self):
+        compiler_type = self.compiler.compiler_type
+
+        if compiler_type == 'msvc':
+            extra_args = []
+            if platform.machine() == "AMD64":
+                extra_args += ["/ARCH:AVX", "/arch:AVX2"]
+        else:
+            extra_args = ['-fno-strict-aliasing']
+            if platform.machine() == "x86_64":
+                extra_args += ["-msse4.1", "-mpclmul"]
+
+        for ext in self.extensions:
+            ext.extra_compile_args = extra_args
+
+        super().build_extensions()
+
+
 ext_modules = [
     Extension(
         "pyfpng",
         sources=["src/fpng-python.cpp", "fpng/src/fpng.cpp"],
         include_dirs=["fpng/src/", numpy.get_include()],
-        extra_compile_args=extra_compile_args,
     )
 ]
 
-# Setup function
 setup(
+    name="pyfpng",
+    version="0.1.0",
     ext_modules=ext_modules,
+    cmdclass={'build_ext': BuildExt},
 )
